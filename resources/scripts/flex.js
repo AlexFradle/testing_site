8e626e36d8e503e42c36e82590ea62eca93f90c4
AlexFradle <alexandergoodall123@gmail.com>
2021-10-11 19:52:55 +0100

    flex area modify

--- resources/scripts/flex.js
+++ resources/scripts/flex.js
@@ -1,26 +1,18 @@
 const randomInt = (min, max) => min + Math.floor(Math.random() * max);
 
-function makeFlex(h, v, n, rw, rh) {
-    let f = document.getElementById("flex");
+function makeFlex(h, v, n, rw, rh, wrap) {
+    const f = document.getElementById("flex");
     f.style.display = "flex";
     f.style.justifyContent = h;
     f.style.alignItems = v;
-    console.log(rw, rh);
+    f.style.flexWrap = wrap;
     f.replaceChildren();
     for (let i = 0; i < n; i++) {
-        let nc = document.createElement("div");
+        const nc = document.createElement("div");
         nc.classList.add("blue");
         nc.classList.add("box");
-        if (rw) nc.style.width = randomInt(10, 100).toString();
-        if (rh) nc.style.height = randomInt(10, 100).toString();
+        if (rw) nc.style.width = `${randomInt(10, 100).toString()}px`;
+        if (rh) nc.style.height = `${randomInt(10, 100).toString()}px`;
         f.appendChild(nc);
     }
 }
-function addOne(i) {
-    if (i != NaN) {
-        return i ++
-    } else {
-      return
-    }
-    let a = '1';
-};
